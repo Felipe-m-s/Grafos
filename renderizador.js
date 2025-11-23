4fb2bbcb1bcb64707419388b3eab01432e797427
Felipe-m-s <fs33143martins@gmail.com>
2025-11-23 00:43:11 -0300

    Alterando forma de leitura de arquivo para uma mais moderna

--- renderizador.js
+++ renderizador.js
@@ -1,3 +1,17 @@
 // Definindo nome do arquivo de grafo
 const nomeDoArquivoDeGrafo = "Grafo.json";
 
+async function carregarDesenharGrafo() {
+    try {
+        const resposta = await fetch(nomeDoArquivoDeGrafo);
+        if (!resposta.ok) {
+            throw new Error(`Erro ao carregar o arquivo: ${resposta.statusText}`);
+        }
+
+        const dadosDoGrafo = await resposta.json();
+    } catch (erro) {
+        console.error("Erro ao carregar o grafo:", erro);
+        container.innerHTML = "<h3>Erro ao carregar o grafo.</h3>";
+        return;
+    }
+}
