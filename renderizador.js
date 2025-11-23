6edd8975281c219302a879a6c9070d19f8aff28f
Felipe-m-s <fs33143martins@gmail.com>
2025-11-23 11:12:11 -0300

    Ininicializando o Cytoscape e configurando o layout COSE

--- renderizador.js
+++ renderizador.js
@@ -9,6 +9,41 @@ async function carregarDesenharGrafo() {
         }
 
         const dadosDoGrafo = await resposta.json();
+
+        const vertices = dadosDoGrafo.vertices.map(v => ({
+            group: 'vertices',
+            data: {
+                id: v.id,
+                x: v.x,
+                y: v.y
+            }
+        }));
+
+        const arestas = dadosDoGrafo.arestas.map((a, index) => ({
+            group: 'arestas',
+            data: {
+                id: `a${index}`,
+                de: a.de,
+                para: a.para,
+                peso: a.peso,
+                label: String(a.peso)
+            }
+        }));
+
+        const elementos = vertices.concat(arestas);
+
+        var cy = cytoscape({
+            container: container,
+            elements: elementos,
+            layout: {
+                name: 'cose',
+                seed: 42,
+                animade: false,
+                paddinf: 30
+            },
+        
+        });
+
     } catch (erro) {
         console.error("Erro ao carregar o grafo:", erro);
         container.innerHTML = "<h3>Erro ao carregar o grafo.</h3>";
