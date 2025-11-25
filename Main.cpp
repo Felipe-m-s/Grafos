7ce77c4f902e61b0756b9adb9c707d85f8cc09d3
Felipe-m-s <fs33143martins@gmail.com>
2025-11-25 09:51:15 -0300

    Implementando os métodos que estavam faltando

--- Main.cpp
+++ Main.cpp
@@ -16,14 +16,19 @@ void imprimirMenu()
 {
     cout << "\nMenu de Opcoes:" << endl;
     cout << "1. Importar grafo de arquivo" << endl;
-    cout << "2. Exibir todas as adjacencias" << endl;
-    cout << "3. Consultar se dois vertices sao adjacentes" << endl;
-    cout << "4. Inserir aresta" << endl;
-    cout << "5. Remover aresta" << endl;
-    cout << "6. Editar coordenada do vertice" << endl;
-    cout << "7. Exportar grafo para arquivo" << endl;
-    cout << "8. Visualizar grafo graficamente" << endl;
-    cout << "9. Sair" << endl;
+    cout << "2. Criar Grafo vazio" << endl;
+    cout << "3. Exibir todas as adjacencias" << endl;
+    cout << "4. Consultar se dois vertices sao adjacentes" << endl;
+    cout << "5. Inserir aresta" << endl;
+    cout << "6. Inserir arestas em conjunto" << endl;
+    cout << "7. Remover aresta" << endl;
+    cout << "8. Editar coordenada do vertice" << endl;
+    cout << "9. Consultar o primeiro adjacente" << endl;
+    cout << "10. Consultar o proximo adjacente" << endl;
+    cout << "11. Consultar lista de adjacentes" << endl;
+    cout << "12. Exportar grafo para arquivo" << endl;
+    cout << "13. Visualizar grafo graficamente" << endl;
+    cout << "0. Sair" << endl;
     cout << "Escolha uma opcao: ";
 }
 
@@ -49,9 +54,22 @@ int main()
             break;
         }
         case 2:
+        {
+            cout << "Criando grafo vazio..." << endl;
+            cout << "Digite o numero de vertices: ";
+            int numVertices;
+            cin >> numVertices;
+            cout << "O grafo sera direcionado? (1 para sim, 0 para nao): ";
+            bool direcionado;
+            cin >> direcionado;
+            meuGrafo.criarVazio(numVertices, direcionado);
+            cout << "Grafo vazio criado com sucesso." << endl;
+            break;
+        }
+        case 3:
             meuGrafo.exibirTodasAsAdjacencias();
             break;
-        case 3:
+        case 4:
         {
             int u, v;
             cout << "Digite os dois vertices para consultar adjacencia (u v): ";
@@ -62,7 +80,7 @@ int main()
                 cout << "Os vertices " << u << " e " << v << " nao sao adjacentes." << endl;
             break;
         }
-        case 4:
+        case 5:
         {
             int u, v, p;
             cout << "Digite os vertices e o peso da aresta a ser inserida (u v p): ";
@@ -70,7 +88,21 @@ int main()
             meuGrafo.inserAresta(u, v, p);
             break;
         }
-        case 5:
+        case 6:
+        {
+            int numArestas;
+            cout << "Quantas arestas deseja inserir? ";
+            cin >> numArestas;
+            for (int i = 0; i < numArestas; ++i)
+            {
+                int u, v, p;
+                cout << "Digite os vertices e o peso da aresta " << (i + 1) << " (u v p): ";
+                cin >> u >> v >> p;
+                meuGrafo.inserAresta(u, v, p);
+            }
+            break;
+        }
+        case 7:
         {
             int u, v;
             cout << "Digite os vertices da aresta a ser removida (u v): ";
@@ -78,7 +110,7 @@ int main()
             meuGrafo.removerAresta(u, v);
             break;
         }
-        case 6:
+        case 8:
         {
             int u, x, y;
             cout << "Digite o vertice e suas novas coordenadas (u x y): ";
@@ -86,21 +118,53 @@ int main()
             meuGrafo.editarCoordenadaDoVertice(u, x, y);
             break;
         }
-        case 7:
+        case 9:
+        {
+            int u;
+            cout << "Digite o vertice para consultar o primeiro adjacente (u): ";
+            cin >> u;
+            int primeiroAdj = meuGrafo.primeiroAdjacenteDoVertice(u);
+            if (primeiroAdj != -1)
+                cout << "O primeiro adjacente do vertice " << u << " e: " << primeiroAdj << endl;
+            else
+                cout << "O vertice " << u << " nao possui adjacentes." << endl;
+            break;
+        }
+        case 10:
+        {
+            int u, atual;
+            cout << "Digite o vertice e o adjacente atual para consultar o proximo adjacente (u atual): ";
+            cin >> u >> atual;
+            int proximoAdj = meuGrafo.proximoAdjacenteDoVertice(u, atual);
+            if (proximoAdj != -1)
+                cout << "O proximo adjacente do vertice " << u << " apos " << atual << " e: " << proximoAdj << endl;
+            else
+                cout << "Nao ha mais adjacentes para o vertice " << u << " apos " << atual << "." << endl;
+            break;
+        }
+        case 11:
+        {
+            int u;
+            cout << "Digite o vertice para consultar a lista completa de adjacentes (u): ";
+            cin >> u;
+            meuGrafo.listaCompletaDeAdjacentesDoVertice(u);
+            break;
+        }
+        case 12:
             meuGrafo.exportar();
             break;
-        case 8:
+        case 13:
             meuGrafo.exportarArquivoJson();
             cout << "Grafo exportado para Grafo.json." << endl;
             system("start Grafo.html"); // Abre o arquivo HTML no navegador padrão
             break;
-        case 9:
+        case 0:
             cout << "Saindo do programa." << endl;
             break;
         default:
             cout << "Opcao invalida! Tente novamente." << endl;
         }
-    } while (opcao != 9);
+    } while (opcao != 0);
 
     return 0;
 }
